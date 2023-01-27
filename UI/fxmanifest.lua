e4e96de395f60affe4ba938d905a676d92bf0e78
Timm TheBlokker <theblokker.dev@gmail.com>
2023-01-27 08:32:16 +0000

    Update UI 0.0.1

--- UI/fxmanifest.lua
+++ UI/fxmanifest.lua
@@ -5,4 +5,6 @@ author 'TheBlokker <github.com/TheBlokker>'
 description 'ServerEinreisesystem'
 version '1.0.0'
 
-
+client_script {
+    "client.lua"
+}
--- teleports/fxmanifest.lua
+++ teleports/fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'TheBlokker <github.com/TheBlokker>'
-description 'ServerEinreisesystem'
+description 'Teleportation in Einreisesytem'
 version '1.0.0'
 
 
